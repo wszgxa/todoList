862e7b4d20b25ad7badfe9db716359b25c5c9c0a
hiluluke <hiluluke@hotmail.com>
2016-03-09 17:58:37 +0800

    修改目录

--- index.jsx
+++ index.jsx
@@ -1,4 +1,4 @@
-require("./assets/index.less");
+require("./assets/less/index.less");
 var React = require('react');
 var ReactDOM = require('react-dom');
 var TodoList = require('./components/TodoList');
