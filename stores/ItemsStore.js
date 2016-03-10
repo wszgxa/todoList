d5c4789aa709a37376d6a6bcb0854d65d49f9685
hiluluke <hiluluke@hotmail.com>
2016-03-10 20:58:05 +0800

    itemsStore ing

--- stores/ItemsStore.js
+++ stores/ItemsStore.js
@@ -2,11 +2,32 @@ var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
 
 var ItemsStore = assign({}, EventEmitter.prototype, {
-  items={},
-  getItems: function () {
+  items: {
+    idMax : 0,
+    itemsList: []
+  },
+  getList: function () {
+    if (this.items.idMax !== 0) {
+        return this.items.itemsList;
+    } else if (window.localStorage.itemsList !== undefined) {
+        var itemsList = window.localStorage.itemsList,
+            itemsOb = JSON.stringify(itemsList);
+        return itemsOb.itemsList;
+        this._initItems(itemsOb);
+    } else {
+        return this.items.itemsList;
+    }
+  },
+  getIdMax: function () {
+      return this.items.idMax;
+  },
+  _initItems: function (Ob) {
+      this.items.idMax = Ob.idMax;
+      this.items.itemsList = Ob.itemsList;
+  },
+  changeItem: function (id) {
+      
   }
-
-
 });
 
 module.exports = TitleStore;
