2bf688f189435ebf56a7f3fec6f9a49238a6b0a4
hiluluke <hiluluke@hotmail.com>
2016-03-13 16:01:46 +0800

    改放修改的方式

--- components/List.jsx
+++ components/List.jsx
@@ -4,27 +4,18 @@ var Tool = require('../tool/Tool');
 var List = React.createClass({
     displayName: 'List',
 
-    // 闭包处理touch事件
-    handleTouchStart: function (e,ss) {
-        var startTime = 0;
-        return function (e) {
-            if (startTime === 0) {
-                startTime = e.timeStamp;
-                // 单击后清除startTime
-                setTimeout(function() {startTime = 0 }, 800);
-            } else{
-                if (e.timeStamp-startTime < 600) {
-                    var tag = e.target.parentNode;
-                    Tool.changeClass(tag,"","editing");
-                    e.target.nextSibling.focus();
-                    startTime = 0;
-                } else {
-                    startTime = 0;
-                }
-            }
-        }
-
-    }(),
+    // 长按修改
+    editStatus: function (e) {
+        var tag = e.target.parentNode;
+        Tool.changeClass(tag,"","editing");
+        e.target.nextSibling.focus();
+    },
+    handleTouchStart: function (e) {
+        timer = setInterval(this.editStatus(e), 2000);
+    },
+    handleTouchEnd:function () {
+        clearTimeout(timer);
+    },
     handleDoubleBclick: function (e) {
         var tag = e.target.parentNode;
         Tool.changeClass(tag,"","editing");
@@ -45,7 +36,7 @@ var List = React.createClass({
         var itemsHtml = this.props.items.map(function(index, i) {
             var item = (
                     <div className="item" key={i}>
-                        <div onTouchStart={that.handleTouchStart} onDoubleClick={that.handleDoubleBclick} data-item={index.id} className="view">
+                        <div onTouchStart={that.handleTouchStart} onTouchEnd={that.handleTouchEnd} onDoubleClick={that.handleDoubleBclick} data-item={index.id} className="view">
                             <input type="checkbox" checked={index.done} onClick={that.props.itemStatusChange}/>
                             <span>{index.content}</span>
                             <a onClick={that.props.removeItem}></a>
